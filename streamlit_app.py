f6c108a64aca2b92bc6c4c8206ab249289ee7a1a
Long3309 <jkrforyou@gmail.com>
2022-11-14 13:11:47 +0700

    img

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 """ ## There is title
     # There is subtitle
 """
-img = cv2.imread("C:/Users/ADMIN/Desktop/cat.jpeg",cv2.COLOR_BGR2RGB)
+img = cv2.imread("cat.jpeg")
 st.image(img)
