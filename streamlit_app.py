8c9ad9c3000ae26ffa340872b8ef1f884ac8d07e
Long3309 <jkrforyou@gmail.com>
2022-11-14 12:46:37 +0700

    OPENCV

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
-import streamlit 
-# import cv2
+import streamlit as st
+import cv2
 import pandas as pd
 """ ## There is title
     # There is subtitle
 """
+img = cv2.imread("C:/Users/ADMIN/Desktop/cat.jpeg",cv2.COLOR_BGR2RGB)
+st.image(img)
